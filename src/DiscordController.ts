fd4bd245d3ab2819f49fd0b725de7b661226287a
Sarumonin <sarumonin@gmail.com>
2019-08-19 03:24:29 -0400

    Update DiscordController.ts

--- src/DiscordController.ts
+++ src/DiscordController.ts
@@ -29,7 +29,7 @@ export class DiscordController
 		largeImageKey: 'ps4_main',
 		largeImageText: 'PlayStation 4',
 		smallImageKey: 'ps4_main',
-		smallImageText: 'PlayStationDiscord ' + (packageJson.version || '')
+		smallImageText: 'Sarumonin'')
 	};
 
 	constructor(console: PlayStationConsole)
